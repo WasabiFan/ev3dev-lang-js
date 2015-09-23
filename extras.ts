bd337f6186cb2f2b9642b607a20f294b46f1dd2f
WasabiFan <wasabifan@outlook.com>
2015-09-23 11:35:39 -0700

    Add LEGO port class

--- extras.ts
+++ extras.ts
@@ -16,17 +16,6 @@ class PowerSupply extends Device {
     private powerDeviceDir = '/sys/class/power_supply/';
     public deviceName: string = 'legoev3-battery';
 
-    get powerProperties(): any {
-        return {
-            currentNow: 'current_now',
-            voltageNow: 'voltage_now',
-            voltageMaxDesign: 'voltage_min_design',
-            voltageMinDesign: 'voltage_max_design',
-            technology: 'technology',
-            type: 'type'
-        };
-    }
-
     constructor(deviceName: string) {
         super();
 
@@ -377,4 +366,153 @@ class LED extends Device {
     public static allOff() {
         this.mixColors(0, 0);
     }
+}
+
+//~autogen js_generic-class-description classes.legoPort>currentClass
+/** 
+ * The `lego-port` class provides an interface for working with input and
+ * output ports that are compatible with LEGO MINDSTORMS RCX/NXT/EV3, LEGO
+ * WeDo and LEGO Power Functions sensors and motors. Supported devices include
+ * the LEGO MINDSTORMS EV3 Intelligent Brick, the LEGO WeDo USB hub and
+ * various sensor multiplexers from 3rd party manufacturers.
+ * 
+ * Some types of ports may have multiple modes of operation. For example, the
+ * input ports on the EV3 brick can communicate with sensors using UART, I2C
+ * or analog validate signals - but not all at the same time. Therefore there
+ * are multiple modes available to connect to the different types of sensors.
+ * 
+ * In most cases, ports are able to automatically detect what type of sensor
+ * or motor is connected. In some cases though, this must be manually specified
+ * using the `mode` and `set_device` attributes. The `mode` attribute affects
+ * how the port communicates with the connected device. For example the input
+ * ports on the EV3 brick can communicate using UART, I2C or analog voltages,
+ * but not all at the same time, so the mode must be set to the one that is
+ * appropriate for the connected sensor. The `set_device` attribute is used to
+ * specify the exact type of sensor that is connected. Note: the mode must be
+ * correctly set before setting the sensor type.
+ * 
+ * Ports can be found at `/sys/class/lego-port/port<N>` where `<N>` is
+ * incremented each time a new port is registered. Note: The number is not
+ * related to the actual port at all - use the `port_name` attribute to find
+ * a specific port.
+ */
+//~autogen
+class LegoPort extends Device {
+    private portDeviceDir = '/sys/class/lego-port/';
+    protected port: string;
+
+    protected _deviceIndex: number = -1;
+    get deviceIndex(): number {
+        return this._deviceIndex;
+    }
+
+    constructor(port: string) {
+        super();
+
+        this.port = port;
+        var rootPath: string;
+
+        try {
+            var availableDevices = fs.readdirSync(this.portDeviceDir);
+            for (var i in availableDevices) {
+                var deviceFile = availableDevices[i];
+
+                rootPath = path.join(this.portDeviceDir, deviceFile);
+                var currentPortName = this.readString('port_name', rootPath);
+
+                var satisfiesCondition = (
+                    (port == ports.OUTPUT_AUTO || port == ports.INPUT_AUTO)
+                    || (port == undefined)
+                    || (currentPortName === port)
+                    );
+
+                if (satisfiesCondition) {
+                    this._deviceIndex = Number(deviceFile.substring('lego-port'.length));
+                    break;
+                }
+            }
+
+            if (this.deviceIndex == -1) {
+                console.log('no device found');
+                this.connected = false;
+                return;
+            }
+        }
+
+        catch (e) {
+            console.log(e);
+            this.connected = false;
+            return;
+        }
+
+        this.connect(rootPath);
+    }
+
+    //PROPERTIES
+    //~autogen js_generic-get-set classes.legoPort>currentClass
+    /**
+     * Returns the name of the driver that loaded this device. You can find the
+     * complete list of drivers in the [list of port drivers].
+     */
+    get driverName(): string {
+        return this.readString("driver_name");
+    }
+
+    /**
+     * Returns a list of the available modes of the port.
+     */
+    get modes(): string[] {
+        return this.readStringArray("modes");
+    }
+
+    /**
+     * Reading returns the currently selected mode. Writing sets the mode.
+     * Generally speaking when the mode changes any sensor or motor devices
+     * associated with the port will be removed new ones loaded, however this
+     * this will depend on the individual driver implementing this class.
+     */
+    get mode(): string {
+        return this.readString("mode");
+    }
+    /**
+     * Reading returns the currently selected mode. Writing sets the mode.
+     * Generally speaking when the mode changes any sensor or motor devices
+     * associated with the port will be removed new ones loaded, however this
+     * this will depend on the individual driver implementing this class.
+     */
+    set mode(value: string) {
+        this.setString("mode", value);
+    }
+    
+    /**
+     * Returns the name of the port. See individual driver documentation for
+     * the name that will be returned.
+     */
+    get portName(): string {
+        return this.readString("port_name");
+    }
+
+    /**
+     * For modes that support it, writing the name of a driver will cause a new
+     * device to be registered for that driver and attached to this port. For
+     * example, since NXT/Analog sensors cannot be auto-detected, you must use
+     * this attribute to load the correct driver. Returns -EOPNOTSUPP if setting a
+     * device is not supported.
+     */
+    set setDevice(value: string) {
+        this.setString("set_device", value);
+    }
+    
+    /**
+     * In most cases, reading status will return the same value as `mode`. In
+     * cases where there is an `auto` mode additional values may be returned,
+     * such as `no-device` or `error`. See individual port driver documentation
+     * for the full list of possible values.
+     */
+    get status(): string {
+        return this.readString("status");
+    }
+
+
+//~autogen
 }
