0a42d02888a924bf23049345475fa425dd9bf243
Wasabi Fan <wasabifan@outlook.com>
2015-11-25 23:18:09 -0800

    Add new sensor types for spec updates

--- motors.ts
+++ motors.ts
@@ -1,5 +1,5 @@
 ﻿//~autogen autogen-header
-    // Sections of the following code were auto-generated based on spec v0.9.3-pre, rev 2. 
+// Sections of the following code were auto-generated based on spec v0.9.3-pre, rev 2.
 
 //~autogen
 
